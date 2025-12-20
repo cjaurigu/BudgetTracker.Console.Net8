4061f1271def90f53ad9b58453e26bbe12601cb3
chris jaurigue <cjaurigu@gmail.com>
2025-12-20 00:44:19 -0700

    Edit/Delete are automatically disabled until you click a row (WPF respects CanExecute)
    Double-click any row → opens the Edit dialog

---
+++ BudgetTracker.Wpf.Net8/Views/TransactionsView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using BudgetTracker.Wpf.Net8.ViewModels;
+
+namespace BudgetTracker.Wpf.Net8.Views
+{
+    public partial class TransactionsView : UserControl
+    {
+        public TransactionsView()
+        {
+            InitializeComponent();
+        }
+
+        private void TransactionsGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (DataContext is not TransactionsViewModel vm) return;
+            if (vm.SelectedTransaction == null) return;
+
+            if (vm.EditSelectedCommand.CanExecute(null))
+                vm.EditSelectedCommand.Execute(null);
+        }
+    }
+}
